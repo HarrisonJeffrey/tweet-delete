687439ba656326d334bf63334030f356a621c0e5
Harrison <UP692768@myport.ac.uk>
2020-11-02 23:03:46 +0000

    Added function to convert string to datetime for each tweet

--- tweet_reader.py
+++ tweet_reader.py
@@ -1,8 +1,26 @@
 import json
 import io
+from datetime import datetime
+import tkinter
 
-with io.open('E:/Work/tweet.js', encoding='utf8') as js_file:
-    data = js_file.read()
-    objs = data[data.find('[') : data.rfind(']')+1]
-    jsonObj = json.loads(objs)
+filepath = 'E:/Work/tweet.js'
 
+
+def convert_js_file(filepath):
+    with io.open(filepath, encoding='utf8') as js_file:
+        data = js_file.read()
+        objs = data[data.find('[') : data.rfind(']')+1]
+        tweets_dict = json.loads(objs)
+
+    return tweets_dict
+
+
+def convert_datetime(tweets_dict):
+    for tweet in tweets_dict:
+        tweet['tweet']['created_at'] = datetime.strptime(tweet['tweet']['created_at'], '%a %b %d %H:%M:%S %z %Y')
+
+    return tweets_dict
+
+
+tweets = convert_js_file(filepath)
+tweets = convert_datetime(tweets)
