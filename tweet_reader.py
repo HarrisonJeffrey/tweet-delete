649da7beeba0d6b37739381478e1e3959dce708c
Harrison <UP692768@myport.ac.uk>
2020-11-05 16:09:19 +0000

    Moved functions into a Tweets class and included get_tweet() function

--- tweet_reader.py
+++ tweet_reader.py
@@ -1,26 +1,43 @@
 import json
 import io
 from datetime import datetime
-import tkinter
+import tweepy
 
 filepath = 'E:/Work/tweet.js'
 
 
-def convert_js_file(filepath):
-    with io.open(filepath, encoding='utf8') as js_file:
-        data = js_file.read()
-        objs = data[data.find('[') : data.rfind(']')+1]
-        tweets_dict = json.loads(objs)
+class Tweets:
+    def __init__(self, tweets_file):
+        self.tweets = self.convert_js_file(tweets_file)
 
-    return tweets_dict
+    def convert_datetime(self, tweets_dict):
+        for tweet in tweets_dict:
+            tweet['tweet']['created_at'] = datetime.strptime(tweet['tweet']['created_at'], '%a %b %d %H:%M:%S %z %Y')
+
+        return tweets_dict
+
+    def convert_js_file(self, filepath):
+        with io.open(filepath, encoding='utf8') as js_file:
+            data = js_file.read()
+            objs = data[data.find('['): data.rfind(']')+1]
+            tweets_dict = json.loads(objs)
+
+        tweets_dict = self.convert_datetime(tweets_dict)
+
+        return tweets_dict
+
+    def get_tweet(self, start, end=None):
+        if end:
+            return self.tweets[start: end]
+        else:
+            return self.tweets[start]
+
+    def get_tweet_by_id(self, id):
+        pass
+
+    def get_tweets_timeframe(self, start, end):
+        pass
 
 
-def convert_datetime(tweets_dict):
-    for tweet in tweets_dict:
-        tweet['tweet']['created_at'] = datetime.strptime(tweet['tweet']['created_at'], '%a %b %d %H:%M:%S %z %Y')
-
-    return tweets_dict
-
-
-tweets = convert_js_file(filepath)
-tweets = convert_datetime(tweets)
+tweets = Tweets(filepath)
+print(tweets.get_tweet(0))
