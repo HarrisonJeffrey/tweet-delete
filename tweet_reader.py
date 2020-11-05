4878fd88a8f5db3c2e30de2e9dc655db03a42975
Harrison <UP692768@myport.ac.uk>
2020-11-05 17:50:45 +0000

    Changed Tweets class to single function that converts to a Pandas dataframe with datetime index

--- tweet_reader.py
+++ tweet_reader.py
@@ -1,43 +1,24 @@
 import json
 import io
-from datetime import datetime
+import pandas as pd
 import tweepy
 
 filepath = 'E:/Work/tweet.js'
 
 
-class Tweets:
-    def __init__(self, tweets_file):
-        self.tweets = self.convert_js_file(tweets_file)
+def convert_js_file(filepath):
+    with io.open(filepath, encoding='utf8') as js_file:
+        data = js_file.read()
+        objs = data[data.find('['): data.rfind(']')+1]
+        tweets_dict = json.loads(objs)
 
-    def convert_datetime(self, tweets_dict):
-        for tweet in tweets_dict:
-            tweet['tweet']['created_at'] = datetime.strptime(tweet['tweet']['created_at'], '%a %b %d %H:%M:%S %z %Y')
+    tweets_dict = [tweet['tweet'] for tweet in tweets_dict]
+    tweets = pd.DataFrame(tweets_dict)
+    tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y')
+    tweets = tweets.set_index('created_at')
 
-        return tweets_dict
-
-    def convert_js_file(self, filepath):
-        with io.open(filepath, encoding='utf8') as js_file:
-            data = js_file.read()
-            objs = data[data.find('['): data.rfind(']')+1]
-            tweets_dict = json.loads(objs)
-
-        tweets_dict = self.convert_datetime(tweets_dict)
-
-        return tweets_dict
-
-    def get_tweet(self, start, end=None):
-        if end:
-            return self.tweets[start: end]
-        else:
-            return self.tweets[start]
-
-    def get_tweet_by_id(self, id):
-        pass
-
-    def get_tweets_timeframe(self, start, end):
-        pass
+    return tweets.sort_values(by='created_at')
 
 
-tweets = Tweets(filepath)
-print(tweets.get_tweet(0))
+tweets = convert_js_file(filepath)
+print(tweets)
