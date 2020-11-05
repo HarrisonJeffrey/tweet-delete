42643629b18704ddf33f5147da9a3bc0a53085ef
Harrison <UP692768@myport.ac.uk>
2020-11-05 20:17:40 +0000

    Added function to get tweets after, before, or between date(s)

--- tweet_reader.py
+++ tweet_reader.py
@@ -15,10 +15,17 @@ def convert_js_file(filepath):
     tweets_dict = [tweet['tweet'] for tweet in tweets_dict]
     tweets = pd.DataFrame(tweets_dict)
     tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y')
-    tweets = tweets.set_index('created_at')
 
     return tweets.sort_values(by='created_at')
 
 
+def get_tweets_by_date(tweets, start=None, end=None):
+    if start and end:
+        return tweets[(tweets['created_at'] >= start) & (tweets['created_at'] <= end)]
+    elif start:
+        return tweets[tweets['created_at'] >= start]
+    elif end:
+        return tweets[tweets['created_at'] <= end]
+
+
 tweets = convert_js_file(filepath)
-print(tweets)
