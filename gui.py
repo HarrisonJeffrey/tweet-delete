67e5927d9f2c33efd259c68224ae2f7a279df7b1
Harrison <UP692768@myport.ac.uk>
2020-12-02 21:11:10 +0000

    Allowed app to change page

--- gui.py
+++ gui.py
@@ -9,8 +9,38 @@ import webbrowser
 from tweet_reader import convert_js_file, get_tweets_by_date
 
 
-class TweetDeleteStart:
-    def __init__(self, master):
+class TweetDelete(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title("Tweet Delete")
+        self.resizable(False, False)
+        self.twitter_logo = ImageTk.PhotoImage(Image.open('Twitter_Logo_Blue.png'))
+        self.iconphoto(False, self.twitter_logo)
+
+        container = tk.Frame(self)
+        container.pack()
+
+        self.frames = {}
+
+        for F in (TweetStartPage, TweetFilterPage, TweetDeletePage):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(TweetStartPage)
+
+    def show_frame(self, contain):
+        frame = self.frames[contain]
+        frame.tkraise()
+
+
+class TweetStartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
         consumer_key = config('consumer_key')
         consumer_secret = config('consumer_secret')
 
@@ -20,39 +50,29 @@ class TweetDeleteStart:
         self.tweets = None
         self.verify = [False, False]
 
-        master.title("Tweet Delete")
-        master.resizable(False, False)
-
-        header_frame = ttk.Frame(master)
-        header_frame.pack()
-
         img = Image.open('Twitter_Logo_Blue.png')
         img = img.resize((100, 100), Image.ANTIALIAS)
         self.twitter_logo = ImageTk.PhotoImage(img)
-        master.iconphoto(False, self.twitter_logo)
-        ttk.Label(header_frame, image=self.twitter_logo).grid(row=0, column=0, pady=5, padx=5)
-        ttk.Label(header_frame, text="Welcome to Tweet Delete!").grid(row=0, column=1, columnspan=3, pady=5, padx=5)
+        ttk.Label(self, image=self.twitter_logo).grid(row=0, column=0, pady=5, padx=5)
+        ttk.Label(self, text="Welcome to Tweet Delete!").grid(row=0, column=1, columnspan=3, pady=5, padx=5)
 
-        main_frame = ttk.Frame(master)
-        main_frame.pack()
-
-        ttk.Label(main_frame, text="To delete tweets we need two things from you!").grid(row=0, column=0, columnspan=10, pady=1, padx=5)
-        ttk.Label(main_frame, text="1) The tweet.js file from your Twitter archive").grid(row=1, column=0, columnspan=10, pady=1, padx=5)
-        ttk.Label(main_frame, text="2) Your access token").grid(row=2, column=0, columnspan=10, pady=1, padx=5)
-        ttk.Label(main_frame, text="Notice: This app doesn't not store any data, so your twitter archive and access tokens are completely private!", wraplength=350).grid(row=4, column=0, columnspan=10, pady=20, padx=5)
-        ttk.Button(main_frame, text='Get access code', command=self.grab_auth).grid(row=5, column=2, pady=10, padx=1)
+        ttk.Label(self, text="To delete tweets we need two things from you!").grid(row=1, column=0, columnspan=10, pady=1, padx=5)
+        ttk.Label(self, text="1) The tweet.js file from your Twitter archive").grid(row=2, column=0, columnspan=10, pady=1, padx=5)
+        ttk.Label(self, text="2) Your access token").grid(row=3, column=0, columnspan=10, pady=1, padx=5)
+        ttk.Label(self, text="Notice: This app doesn't not store any data, so your twitter archive and access tokens are completely private!", wraplength=350).grid(row=5, column=0, columnspan=10, pady=20, padx=5)
+        ttk.Button(self, text='Get access code', command=self.grab_auth).grid(row=6, column=0, pady=10, padx=1)
         self.verifier = tk.StringVar()
-        ttk.Label(main_frame, text="Access Code: ").grid(row=5, column=3)
-        ttk.Entry(main_frame, width=10, textvariable=self.verifier).grid(row=5, column=4, pady=5, padx=1)
+        ttk.Label(self, text="Access Code: ").grid(row=6, column=2)
+        ttk.Entry(self, width=10, textvariable=self.verifier).grid(row=6, column=3, pady=5, padx=1)
 
-        ttk.Button(main_frame, text="Load tweet.js", command=self.load_file).grid(row=6, column=0, columnspan=10, pady=5, padx=2)
+        ttk.Button(self, text="Load tweet.js", command=self.load_file).grid(row=7, column=0, columnspan=10, pady=5, padx=2)
         self.loaded_message = tk.StringVar()
-        ttk.Label(main_frame, textvariable=self.loaded_message, wraplength=300).grid(row=7, column=0, columnspan=10)
+        ttk.Label(self, textvariable=self.loaded_message, wraplength=300).grid(row=8, column=0, columnspan=10)
 
         self.message = tk.StringVar()
-        ttk.Label(main_frame, textvariable=self.message, wraplength=300).grid(row=8, column=0, columnspan=10, pady=5, padx=1)
+        ttk.Label(self, textvariable=self.message, wraplength=300).grid(row=9, column=0, columnspan=10, pady=5, padx=1)
 
-        ttk.Button(main_frame, text="Continue", command=self.next_page).grid(row=9, column=0, columnspan=10, padx=5, pady=5)
+        ttk.Button(self, text="Continue", command=self.next_page).grid(row=10, column=0, columnspan=10, padx=5, pady=5)
 
     def grab_auth(self):
         if self.redirect_url is None:
@@ -99,14 +119,15 @@ class TweetDeleteStart:
             self.message.set("Something isn't right. Have you loaded your tweets.js archive and verified access?")
         else:
             self.message.set("")
-            print("hi")
+            self.controller.show_frame(TweetFilterPage)
 
 
-def main():
-    root = tk.Tk()
-    app = TweetDeleteStart(root)
-    root.mainloop()
+class TweetFilterPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        ttk.Label(self, text="test")
 
 
-if __name__ == '__main__':
-    main()
+class TweetDeletePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
--- tweet_delete.py
+++ tweet_delete.py
@@ -1,37 +1,9 @@
-from decouple import config
-import webbrowser
-import tweepy
-from tweet_reader import convert_js_file, get_tweets_by_date
-
-
-def tweepy_auth():
-    consumer_key = config('consumer_key')
-    consumer_secret = config('consumer_secret')
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-    try:
-        redirect_url = auth.get_authorization_url()
-    except tweepy.TweepError:
-        print('Error! Failed to get request token.')
-
-    webbrowser.open(redirect_url, new=0, autoraise=True)
-    verifier = input('Verifier: ')
-
-    try:
-        auth.get_access_token(verifier)
-    except tweepy.TweepError:
-        print('Error! Failed to get access token.')
-
-    api = tweepy.API(auth)
-    return api
-
+import tkinter as tk
+import gui
 
 def main():
-    api = tweepy_auth()
-
-    filepath = 'tweet.js'
-    tweets = convert_js_file(filepath)
+    app = gui.TweetDelete()
+    app.mainloop()
 
     # id = '1333848716311224320'
     # api.destroy_status(id)
