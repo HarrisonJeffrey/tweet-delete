482cd2b987264fe59e9ed9054927dcf96d362491
Harrison <UP692768@myport.ac.uk>
2020-12-03 17:45:06 +0000

    Fixed created_at column being timestamp and changed it to .dt.date

--- tweet_reader.py
+++ tweet_reader.py
@@ -11,7 +11,7 @@ def convert_js_file(filepath):
 
     tweets_dict = [tweet['tweet'] for tweet in tweets_dict]
     tweets = pd.DataFrame(tweets_dict)
-    tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y')
+    tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y').dt.date
 
     return tweets.sort_values(by='created_at')
 
