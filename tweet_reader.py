8119ddad1ffbf933d7a37550cb0bb3e6cb4297f3
Harrison <UP692768@myport.ac.uk>
2020-12-04 17:15:07 +0000

    Removed redundant columns, changed 'created_at' to datetime and added 'date' as dt.date

--- tweet_reader.py
+++ tweet_reader.py
@@ -11,9 +11,11 @@ def convert_js_file(filepath):
 
     tweets_dict = [tweet['tweet'] for tweet in tweets_dict]
     tweets = pd.DataFrame(tweets_dict)
-    tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y').dt.date
+    tweets = tweets.drop(labels=['retweeted', 'source', 'entities', 'display_text_range', 'favorite_count', 'id_str', 'truncated', 'retweet_count', 'favorited', 'lang'], axis=1)
+    tweets['created_at'] = pd.to_datetime(tweets['created_at'], format='%a %b %d %H:%M:%S %z %Y')
+    tweets['date'] = tweets['created_at'].dt.date
 
-    return tweets.sort_values(by='created_at')
+    return tweets.sort_values(by='date')
 
 
 def get_tweets_by_date(tweets, start=None, end=None):
